fec3a9ac8449bbb84e325c6a7a4e823bd4188888
PreetiDiwakarGitHub <p208016@gmail.com>
2024-04-24 16:11:32 +0530

    first commit

---
+++ class.js
@@ -0,0 +1,145 @@
+// question  1
+
+
+//  let arr = [1,2,3,4,5,6,7];
+//  let sum =0;
+//  for(let j=0; j<arr.length; j++){
+//     sum = sum + arr[j];
+//  }
+//  console.log(sum);
+
+// question 1 \\\\\\\\\
+
+// let arr = [1,2,3,4,5,6,7];
+// function sum(){
+//     let sumnum=0;
+//     for(let j=0; j<arr.length; j++){
+//         sumnum = sumnum + arr[j];
+//     }
+//     return(sumnum);
+// }
+// console.log(sum());
+
+
+// question  2 \\\\\\\\\\\\\\\\\\\
+
+// let arr = [1,2,3,4,5];
+// function average(){
+//     let sumnum=0;
+//     let d = arr.length;
+//     for(let j=0; j<d; j++){
+//         sumnum = sumnum + arr[j];
+//     }
+//     let p = sumnum/d;
+//     return(p);
+// }
+// console.log(average());
+
+
+// question   3 \\\\\\\\\\\\\\\\\\\\\
+
+
+// let arr = [1,2,3,4,5,6,7];
+// function maximum(){
+//     let max = arr[0];
+//     for(let j=1; j<arr.length; j++){
+//         if(max<arr[j]){
+//             max=arr[j];
+//         }
+
+//     }
+//     return (max);
+// }
+// console.log(maximum());
+  
+
+// question  4 \\\\\\\\\\\\\\\\\\\\\\\\
+
+// let arr = [1,2,3,4,5,6,7];
+// function maximum(){
+//         let min = arr[0];
+//         for(let j=1; j<arr.length; j++){
+//             if(min>arr[j]){
+//                 min=arr[j];
+//             }
+    
+//         }
+//         return (min);
+//     }
+//     console.log(maximum());
+      
+
+// question   5  \\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// let arr = [1,7,3,7,5,6,7];
+// function  inc(){
+//     let i=7;
+//     for(let j=0; j<arr.length; j++){
+//         if(i==arr[j]){
+//             return(j);
+//         }
+//     }
+//     return(-1);
+// }
+// console.log(inc());
+
+// question  6 \\\\\\\\\\\\\\\\\\\\\
+
+
+// sort question \\\\\
+  
+// let arr = [4,1,3,5,6,7,8];
+// let p = arr.sort();
+
+// console.log(p);
+
+// let arr = [4,3,5,1,6,7];
+// function arrs(){
+//     let p = arr.sort();
+//     return(p);
+    
+// }
+// console.log(arrs());
+
+
+// question   7   \\\\\\\\\\\\\\\\\\\\\\
+// let number = [1,2,3,4,5,6,7,8];
+// function even( num ){
+//     return num % 2==0;
+// }
+// console.log(number.filter(even));
+
+
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// function even(numbers) {
+//     let evenNumbers = [];
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] % 2 === 0) {
+//             evenNumbers.push(numbers[i]);
+//         }
+//     }
+//     return evenNumbers;
+// }
+
+// console.log(even(numbers));
+
+// let user={
+//     firstname: "preeti",
+//     lastname: "diwakar",
+//     myfullname(){
+//        return this.firstname + this.lastname
+//     },
+// }
+// console.log(user.myfullname());
+
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/array_question.git
+// git push -u origin main
+
